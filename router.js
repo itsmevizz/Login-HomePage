780f9e0a9e59832313e213500c1a8160508518cb
itsmevizz <vizzlak32@gmail.com>
2022-04-18 11:20:17 +0530

    compleated-ok

--- router.js
+++ router.js
@@ -1,46 +1,50 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
 const credential = {
-    username : 'admin',
-    password : 'admin'
-}
+  username: "admin",
+  password: "admin",
+};
 //login user............
-router.post('/login',(req,res)=>{
-    if(req.body.username==credential.username && req.body.password==credential.password ){
-       if( req.session.user=req.body.username){
-        res.redirect('/route/home')
-       }
-        // res.end('login successfull');    
-    }if(req.session.user!=req.body.username){
-        if(req.body.username=="" || req.body.password==""){
-            res.render("base",{invalid:"username and password must not be empty"})
-        }else{
-            res.render("base",{invalid:"Invalid Username or Password"})
-        }
+router.post("/login", (req, res) => {
+  if (
+    req.body.username == credential.username &&
+    req.body.password == credential.password
+  ) {
+    req.session.user = req.body.username;
+    if (req.session.user) {
+      res.redirect("/route/home");
     }
+  } else {
+    if (req.body.username == "" || req.body.password == "") {
+      req.session.err = "Username and password must not be empty";
+      res.redirect("/");
+    } else {
+      req.session.err = "Invalid Username or Password";
+      res.redirect("/");
+    }
+  }
 });
 
 //route for Home
-router.get('/home',(req, res)=>{
-    if(req.session.user){
-        res.render('home',{user:req.session.user})
-    }else{
-        res.render('base')
-    }
-})
+router.get("/home", (req, res) => {
+  if (req.session.user) {
+    res.render("home", { user: req.session.user });
+  } else {
+    res.render("base");
+  }
+});
 
 //route for logout
-router.get('/logout',(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err){
-            console.log(err);
-            res.send('Error')
-        }else{
-            res.render('base',{title:'',logout:'Logout successfully.'})
-        }
-    })
-})
-
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-const express=require('express');
-const path=require('path');
-const bodyparser=require('body-parser');
-const session=require('express-session');
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const session = require("express-session");
 
+const { v4: uuidv4 } = require("uuid");
+const router = require("./router");
+const app = express();
 
-const{v4:uuidv4}=require('uuid');
-const router=require('./router')
-const app=express();
-
-
-const port=process.env.PORT||4040;
+const port = process.env.PORT || 4040;
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}))
-app.set('view engine','ejs') 
-app.use((req,res,next)=>{
-    if(!req.user){
-        res.header('cache-control','private,no-cache,no-store,must revalidate')
-        res.header('Express','-1')
-        res.header('paragrm','no-cache')
-      
-    }
-    next();
-})
-app.use(session({
-    secret:uuidv4(),
-    resave:false,
-    saveUninitialized:true
-}));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use((req, res, next) => {
+  if (!req.user) {
+    res.header(
+      "Cache-Control",
+      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+  }
+  next();
+});
 
-app.use('/route',router)
+app.use(
+  session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use("/route", router);
 //home route
-app.get('/',(req,res)=>{
-    if(req.session.user){
-        res.render("home",{user:req.session.user})
-    }else{
-        res.render('base',{title:'log-in system'})
-    }
-    
-}) 
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    res.render("home", { user: req.session.user });
+  } else {
+    var invalid = req.session.err;
+    req.session.err = null;
+    res.render("base", { title: "log-in system", invalid });
+  }
+});
 
-
-
-app.listen(port,()=>{console.log('Running the server at http://localhost:4040')});
+app.listen(port, () => {
+  console.log("Running the server at http://localhost:4040");
+});
